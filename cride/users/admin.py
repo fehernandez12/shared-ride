8cba5ed5c3f738900088478ceb47d7e3417d365a
Felipe Hernández <feehernandezba@gmail.com>
2022-05-15 22:38:51 -0500

    Updated user admin to include phone number

--- cride/users/admin.py
+++ cride/users/admin.py
@@ -11,7 +11,7 @@ from cride.users.models import User, Profile
 class CustomUserAdmin(UserAdmin):
     """User model admin."""
 
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_client')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'phone_number', 'is_staff', 'is_client')
     list_filter = ('is_client', 'is_staff', 'created', 'modified')
 
 
