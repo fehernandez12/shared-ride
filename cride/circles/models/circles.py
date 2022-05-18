8e5b6c15d2bd2ba189e8c962a11c7c34d9fc6d6b
Felipe Hernández <feehernandezba@gmail.com>
2022-05-18 13:53:02 -0500

    Auto generated slugs for circles

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -59,7 +59,7 @@ class Circle(CRideModel):
         """Return circle name."""
         return self.name
 
-    def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    def random_string_generator(size, chars=string.ascii_lowercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
 
     def save(self, *args, **kwargs):
