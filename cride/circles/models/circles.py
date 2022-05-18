de674aca0958301f545a4a2c167eb98a50d88513
Felipe Hernández <feehernandezba@gmail.com>
2022-05-18 14:42:34 -0500

    Auto generated slugs for circles

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -38,7 +38,6 @@ class Circle(CRideModel):
     )
 
     is_public = models.BooleanField(
-        default=True,
         help_text='Public circles are listed in the main page so everyone know about their existence.'
     )
 
