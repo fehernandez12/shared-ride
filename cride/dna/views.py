4e72254a8235e5c4c3a73235ae15e937785ac3f0
Felipe Hernández <feehernandezba@gmail.com>
2023-03-02 10:17:33 -0500

    Permissions for dna views

--- cride/dna/views.py
+++ cride/dna/views.py
@@ -1,10 +1,16 @@
 from cride.dna.serializers import ProteinSerializer
 from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProteinViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = ProteinSerializer
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        permissions = [IsAuthenticated]
+        return [p() for p in permissions]
 
     def create(self, request, *args, **kwargs):
         """Handle protein creation from DNA string."""
