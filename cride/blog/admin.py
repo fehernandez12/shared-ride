3ecdac12ebd3e8d022ad5c80af91284a9df9dca2
Felipe Hernández <feehernandezba@gmail.com>
2023-05-12 13:17:23 -0500

    Contact form

--- cride/blog/admin.py
+++ cride/blog/admin.py
@@ -1,26 +1,29 @@
 from django.contrib import admin
 
-from cride.blog.models import Post, Status, Comment
+from cride.blog.models import Post, Status, Comment, Contact
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     """Post admin."""
-    list_display = ('title', 'published', 'status')
-    list_display_links = ('title',)
-    list_editable = ('status',)
-    search_fields = ('title', 'body')
-    list_filter = ('status', 'created', 'modified')
+
+    list_display = ("title", "published", "status")
+    list_display_links = ("title",)
+    list_editable = ("status",)
+    search_fields = ("title", "body")
+    list_filter = ("status", "created", "modified")
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     """Comment admin."""
-    list_display = ('name', 'post', 'active')
-    list_display_links = ('name', 'post')
-    list_editable = ('active',)
-    search_fields = ('name', 'email', 'comment')
-    list_filter = ('active', 'created', 'modified')
+
+    list_display = ("name", "post", "active")
+    list_display_links = ("name", "post")
+    list_editable = ("active",)
+    search_fields = ("name", "email", "comment")
+    list_filter = ("active", "created", "modified")
 
 
 admin.site.register(Status)
+admin.site.register(Contact)
